f4b6cd8c886c5452badacffe88b1e1d03fbac058
Hendrek Rodrigues <llhendrek@gmail.com>
2025-09-29 03:35:36 -0400

    feat: frontend | grid 5 cards in page

--- front-end/src/pages/PlaylistPage.jsx
+++ front-end/src/pages/PlaylistPage.jsx
@@ -78,7 +78,7 @@ export default function PlaylistPage() {
         )}
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         {playlist.musics.map((song) => (
           <MusicCard
             key={song.number}
